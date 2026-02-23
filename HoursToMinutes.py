de5364e75489fab7f9a17a1661d9fb729a3f81f5
Jorge Brown <jorge.a.brown@outlook.com>
2026-02-23 10:41:56 -0800

    Created hourstominutes

---
+++ HoursToMinutes.py
@@ -0,0 +1,7 @@
+hours = int(input('Enter hours:\n'))
+minutes = int(input('Enter minutes:\n'))
+
+hours_toMinutes = hours * 60
+total_minutes = hours_toMinutes + minutes
+
+print(total_minutes, 'minutes')
