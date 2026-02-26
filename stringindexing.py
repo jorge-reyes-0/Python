5a446e8b5a49a6b7632e49cb243389b51d49c57a
Jorge Brown <jorge.a.brown@outlook.com>
2026-02-26 12:13:46 -0800

    Created string indexing

---
+++ stringindexing.py
@@ -0,0 +1,17 @@
+alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+print(
+    alphabet[0],
+    alphabet[19],
+    alphabet[19],
+    alphabet[0],
+    alphabet[2],
+    alphabet[10]
+)
+
+print(
+    alphabet[-14],
+    alphabet[-26],
+    alphabet[-9],
+    alphabet[-8]
+)
