3eff784724f84639ba9b6e596f61b180a942150c
Jorge Brown <jorge.a.brown@outlook.com>
2026-02-26 09:28:06 -0800

    Created madlibs

---
+++ madlibs.py
@@ -0,0 +1,12 @@
+identity = input('Enter someones name: ')
+
+food = input('Enter a type of food: ')
+
+adjective = input('Enter an adjective: ')
+
+time = input('Enter a time period: ')
+print()
+
+print('My', identity, 'says eating', food)
+print('will make me more', adjective)
+print('so now I eat them every', time)
