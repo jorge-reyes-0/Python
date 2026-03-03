e37097eb2392784ed0a8ee2f70f3f70a1896e721
Jorge Brown <jorge.a.brown@outlook.com>
2026-03-03 10:44:21 -0800

    Updated dictionary

--- dictionary.py
+++ dictionary.py
@@ -1,35 +1,44 @@
-stats = {
-    'Jan': 34,
-    'Feb': 28,
-    'Mar': 41,
-    'Apr': 30
+sales = {
+    'Jan': 2001,
+    'Feb': 1412,
+    'Mar': 3000,
+    'Apr': 500,
+    'May': 10000,
+    'June': 100,
+    'July': 679,
+    'Aug': 301221,
+    'Sep': 1,
+    'Oct': 5,
+    'Nov': 890,
+    'Dec': 2320
 }
 
-highest = list(stats.values())[0]
-highest_key = list(stats.keys())[0]
-lowest = list(stats.values())[0]
-lowest_key = list(stats.keys())[0]
+highest = list(sales.values())[0]
+highest_month = list(sales.keys())[0]
+
+least = list(sales.values())[0]
+least_month = list(sales.keys())[0]
 
 total = 0
 count = 0
 
-for k in sorted(stats):
-    print(k, stats[k])
+for k in sorted(sales):
+    print(k, sales[k])
 
-    total += stats[k]
+    total += sales[k]
     count += 1
 
-    if stats[k] > highest:
-        highest = stats[k]
-        highest_key = k
+    if sales[k] > highest:
+        highest = sales[k]
+        highest_month = k
 
-    if stats[k] < lowest:
-        lowest = stats[k]
-        lowest_key = k
+    if sales [k] < least:
+        least = sales[k]
+        least_month = k
 
 average = total / count
 
-print(f'Highest: {highest_key} {highest}')
-print(f'Lowest: {lowest_key} {lowest}')
-print(f'Total: {total}')
-print(f'Average: {average}')
+print(f'The best sales month was {highest_month} with ${highest} in sales')
+print(f'The worst sales month was {least_month} with ${least} in sales')
+print(f'Total sales for the year is ${total}')
+print(f'Average sales for 1 month was ${average}')
