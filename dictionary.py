772e00d64c92c3a57f425b427431bff60169d0d1
Jorge Brown <jorge.a.brown@outlook.com>
2026-03-03 08:40:20 -0800

    Added average to dictionary

--- dictionary.py
+++ dictionary.py
@@ -9,12 +9,15 @@ highest = list(stats.values())[0]
 highest_key = list(stats.keys())[0]
 lowest = list(stats.values())[0]
 lowest_key = list(stats.keys())[0]
+
 total = 0
+count = 0
 
 for k in sorted(stats):
     print(k, stats[k])
 
     total += stats[k]
+    count += 1
 
     if stats[k] > highest:
         highest = stats[k]
@@ -24,6 +27,9 @@ for k in sorted(stats):
         lowest = stats[k]
         lowest_key = k
 
+average = total / count
+
 print(f'Highest: {highest_key} {highest}')
 print(f'Lowest: {lowest_key} {lowest}')
 print(f'Total: {total}')
+print(f'Average: {average}')
