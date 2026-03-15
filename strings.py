9e618cf976a3ede60ece1f7cd25e16973076cbbe
jorgeabrown <jorge.a.brown@outlook.com>
2026-03-15 15:44:51 -0500

    Created strings

---
+++ strings.py
@@ -0,0 +1,7 @@
+my_str = 'The cat jumped the brown cow'
+animal = my_str[4:7]
+
+print(f'The animal is a {animal}')
+
+my_str = 'The fox jumped over the brown llama'
+print(f'The animal is still a {animal}')
