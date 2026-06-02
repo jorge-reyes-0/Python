67acd0043cb7729e0349feeeefd77c1d54743964
Jorge Brown <jorge.a.brown@outlook.com>
2026-06-02 06:32:31 -0700

    Created index formatting and number formatting

---
+++ D522/index_formatting.py
@@ -0,0 +1,5 @@
+# Index based formatting
+
+message = "{0} is a {1} programming language.".format("Python", "versatile")
+
+print(message)
---
+++ D522/number_formatting.py
@@ -0,0 +1,7 @@
+# Number formatting
+
+value = 1234567.89
+
+formatted_value = "Formatted value: {:,.2f}".format(value)
+
+print(formatted_value)
