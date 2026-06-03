b5db5a3fd6a1038d0630547ca30f89ae17f91152
Jorge Brown <jorge.a.brown@outlook.com>
2026-06-03 10:46:52 -0700

    Added reversing in sorting IPs

--- D522/sorting_IPs.py
+++ D522/sorting_IPs.py
@@ -7,3 +7,9 @@ ip_addresses = ["192.168.1.3", "192.168.1.1.", "192.168.1.2"]
 ip_addresses.sort()
 
 print(ip_addresses)
+
+# Reverse the order of IP addresses
+
+ip_addresses.reverse()
+
+print(ip_addresses)
