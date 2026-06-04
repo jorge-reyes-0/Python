c94c4af9821293a1b59469239edfda70c10df2ff
Jorge Brown <jorge.a.brown@outlook.com>
2026-06-04 07:26:15 -0700

    Creating tuple packing and constructor

--- D522/tuple_packing.py
+++ D522/tuple_packing.py
@@ -9,3 +9,35 @@ ip_address, device_type, username, password = network_device
 # Use the unpacked values
 
 print(f'Connecting to device {device_type} at {ip_address} using username {username}')
+
+###############################################################################################
+
+# Tuple Length 
+
+# Creating the tuple
+
+network_devices = ('Router1', 'Switch1', 'Firewall1', 'Server1')
+
+# Determining the length of the tuple
+
+num_devices = len(network_devices)
+
+print(f'The number of network devices is: {num_devices}')
+
+##############################################################################################
+
+# Tuples with a single item
+
+# Define a tuple with a single item
+
+username = ('admin',)
+
+# Unpack the tuple
+
+(username,) = username
+
+# Use the unpacked value
+
+print(f'Username: {username}')
+
+
