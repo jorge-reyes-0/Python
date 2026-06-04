af714b934e3a5f0aa3c512250406ee4053c766a8
Jorge Brown <103546482+jorgeabrown@users.noreply.github.com>
2026-06-04 08:03:21 -0700

    Delete D522/Writing_and_Appending.py

--- D522/Writing_and_Appending.py
+++
@@ -1,7 +0,0 @@
-with open("report.txt", "w") as f:
-    f.write("System check complete\n")   # overwrites file
-
-with open("log.txt", "a") as f:
-    f.write("New entry added\n")         # adds to end of file
-
-# Unlike print(), f.write() does NOT add a newline automatically. You must use \n yourself
