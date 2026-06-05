6b6e73638489ab2eeef0acac9f21900fc9432b93
Jorge Brown <jorge.a.brown@outlook.com>
2026-06-05 08:46:28 -0700

    Created sets

---
+++ D522/sets.py
@@ -0,0 +1,13 @@
+# Example of a set
+
+network_devices = {'Switch', 'Router', 'Firewall'}
+new_devices = {'Firewall', 'Load Balancer'}
+# Add new devices to the network
+
+network_devices = network_devices.union(new_devices)
+
+print(network_devices)
+
+##########################################################################
+
+# Creating a set
