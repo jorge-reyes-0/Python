e31c418c0aedc2f1435ccc79260a3e7219e251c1
Jorge Brown <jorge.a.brown@outlook.com>
2026-06-09 08:30:45 -0700

    Started if else

---
+++ D522/if_Else.py
@@ -0,0 +1,35 @@
+# Example of a simple network automation using if / else
+
+device_status = 'online'
+
+# Check if the device is online
+
+if device_status == 'online':
+    print('Device is online. Proceed with configuration')
+else:
+    print('Device is offline. Unable to perform configuration')
+
+############################################################################
+
+# Branches
+# A branch is a set of statements that are only executed when certain conditions are met.
+# In flowcharts, a decision creates two branches. If the decision is true, then the first
+# branch executes. Otherwise, the second branch executes.
+
+############################################################################
+
+# Pseudocode
+# Is a high-level description of an algorithm or a program written in a way that is easy for
+# humans to understand. 
+
+# Pseudocode for a if/else statement
+
+if condition is true:
+    # Code to be executed if the condition is true
+    perform action A
+else:
+    # Code to be executed if the condition is false
+    perform action B
+
+############################################################################
+
