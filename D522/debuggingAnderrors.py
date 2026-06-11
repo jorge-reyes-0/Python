1265c43dc8e2a01eb07ecefa2cb10753bd5c2909
Jorge Brown <jorge.a.brown@outlook.com>
2026-06-11 08:09:54 -0700

    Created debugging

---
+++ D522/debuggingAnderrors.py
@@ -0,0 +1,36 @@
+# Debugging and Fixing Errors
+
+# Syntax Errors
+    # These occur when the Python parser is unable to understand a line of code.
+    # Usually the result of typos or misunderstandings about the syntax.
+
+# Runtime Errors
+    # Occur during the execution of a program. Often caused by operations that are
+    # mathematically illegal, such as division by zero, or by attempting to access
+    # a resource that isnt available, such as reading a file that doesnt exist.
+
+# Semantic Errors
+    # These are the most insidious errors as the program runs without crashing, but
+    # it doesnt produce the expected output. Could be due to an error in the programs logic
+
+###########################################################################################
+
+# Python Debugging
+
+# Python debugging is a systematic process of finding and reducing the number of bugs or defects
+# in a python program.
+
+# Basic steps of python debugging:
+    # Understanding the problem
+    # Isolating the problem
+    # Using a debugger
+    # Fixing the error
+    # Testing the solution
+
+# Common debugging techniques
+    # Print statements
+    # Using a debugger
+    # Code review
+    # Unit testing
+    # Logging
+    # Profiling
