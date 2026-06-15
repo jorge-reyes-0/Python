c3c5fc59f1ab5fc53689cd8d0792f66846606f6b
Jorge Brown <jorge.a.brown@outlook.com>
2026-06-15 08:09:55 -0700

    Created module stuff

---
+++ D522/pythonModules_scripts_Packages.py
@@ -0,0 +1,32 @@
+# Python Modules
+    # A python module is a file containing Python definitions and statements, which provides
+    # a way to logically organize Python code. 
+
+    # Netmiko
+        # For SSH connections to routers and switches
+    # NAPALM
+        # Provides a set of functions to interact with different network device OS's
+    # Ansible
+        # An open source software provisioning, configuration management, and application
+        # deployment tool
+    # Paramiko
+        # For implementing SSHv2 protocol
+    # Exscript
+        # Automates Telnet and SSH connections to remote hosts
+
+# Python Packages
+    # A python package is a collection of Python modules, or files containing Python code,
+    # organized in a directory heirarchy. Allows for a structured, modular approach to programming
+    # Packages are installed using package managers like pip
+
+# Python Libraries
+    # Libraries are a reusable chunk of code that you may want to include in programs / scripts
+    # Provides pre-written functionality, reducing the amount of code you need to write
+    # Offer pre-built modules for common networking tasks. 
+
+        # Requests
+            # For making HTTP requests
+        # BeautifulSoup
+            # For parsing HTML and XML documents
+        # Scapy
+            # For crafting, sending and receiving packets
